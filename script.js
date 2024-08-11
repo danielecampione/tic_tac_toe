769737c3f5785fddae3471d21357422422d887c7
Daniele Campione <nonametekno@gmail.com>
2024-08-11 22:11:04 +0200

    v001a
    v001a

---
+++ script.js
@@ -0,0 +1,64 @@
+const cells = document.querySelectorAll('[data-cell]');
+const board = document.getElementById('board');
+const restartButton = document.getElementById('restartButton');
+let isXTurn = true;
+
+const winningCombinations = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+];
+
+function handleClick(e) {
+    const cell = e.target;
+    const currentClass = isXTurn ? 'x' : 'o';
+    placeMark(cell, currentClass);
+    if (checkWin(currentClass)) {
+        setTimeout(() => alert(`${currentClass.toUpperCase()} Wins!`), 10);
+    } else if (isDraw()) {
+        setTimeout(() => alert('Draw!'), 10);
+    } else {
+        swapTurns();
+    }
+}
+
+function placeMark(cell, currentClass) {
+    cell.classList.add(currentClass);
+}
+
+function swapTurns() {
+    isXTurn = !isXTurn;
+}
+
+function checkWin(currentClass) {
+    return winningCombinations.some(combination => {
+        return combination.every(index => {
+            return cells[index].classList.contains(currentClass);
+        });
+    });
+}
+
+function isDraw() {
+    return [...cells].every(cell => {
+        return cell.classList.contains('x') || cell.classList.contains('o');
+    });
+}
+
+function startGame() {
+    cells.forEach(cell => {
+        cell.classList.remove('x');
+        cell.classList.remove('o');
+        cell.removeEventListener('click', handleClick);
+        cell.addEventListener('click', handleClick, { once: true });
+    });
+    isXTurn = true;
+}
+
+restartButton.addEventListener('click', startGame);
+
+startGame();
